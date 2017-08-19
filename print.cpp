057d53ce85a0d748d06082d86db0333b2248d3b4
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2017-08-18 17:54:41 -0700

    Add print function

---
+++ print.cpp
@@ -0,0 +1,8 @@
+#include "print.h"
+
+#include <iostream>
+namespace tc {
+    void print(int i) {
+        std::cout << "Hello, World " << i << "!" << std::endl;
+    }
+}
---
+++ print.h
@@ -0,0 +1,6 @@
+#ifndef TC_SHARED_LIBRARY_H
+#define TC_SHARED_LIBRARY_H
+namespace tc {
+    void print(int i);
+}
+#endif
